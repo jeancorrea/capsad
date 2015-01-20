c6307da28f3d3d8893a69978d28aada6528e619d
Jean Correa <jean.do.violao@gmail.com>
2015-01-20 15:49:17 -0200

    Criado topo de página, adicionado folha de estilo e pasta img

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
 <!DOCTYPE html>
-<html>
+<!--[if IE 9]><html class="lt-ie10" lang="pt-br" > <![endif]-->
+<html lang="pt-br">
 <head>
+	<meta charset="UTF-8">
+	<title>CAPSad Digital</title>
+	<link rel="stylesheet" href="style.css">
+	<script src="script.js"></script>
+	<link rel="icon" href="img/favicon.ico" />
 </head>
 
 <body>
+	<div class="top">
+		<div id="logo">
+			<img src="img\ancora.png" /><span id="titulo">CAPSad Digital</span><!--titulo-->
+		</div><!--logo-->
+		<div id="descricao">
+			CAPSad Porto<br />
+			Rua Velho Campos, 1.071, Centro, Macaé - RJ<br />
+			Tel.: (22) 2796-1295 / E-mail: capsadmacae@gmail.com	
+		</div><!--descricao-->
+	</div><!--top-->	
 </body>
 </html>
